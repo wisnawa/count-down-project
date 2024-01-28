b2357a718adb194567ff2ff5c4a43249ca6a65c4
wisnawa <putu.wisnawa@gmail.com>
2024-01-28 20:05:25 +0800

    creating new project vanilla JS

---
+++ script.js
@@ -0,0 +1,19 @@
+const countdownDate = new Date("FEB 14, 2024 00:00:00").getTime();
+
+const x = setInterval(function () {
+  const now = new Date().getTime();
+
+  const distance = countdownDate - now;
+
+  const days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  const hours = Math.floor(
+    (distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
+  );
+  const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  const seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+  document.getElementById("days").innerHTML = days;
+  document.getElementById("hours").innerHTML = hours;
+  document.getElementById("minutes").innerHTML = minutes;
+  document.getElementById("seconds").innerHTML = seconds;
+}, 1000);
